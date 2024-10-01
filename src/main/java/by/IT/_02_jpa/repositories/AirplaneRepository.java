373820a2efeed467501a253dd9f12bac9c056c9f
Asus <vaxa2320@gmail.com>
2024-10-01 15:43:35 +0300

    user011

--- src/main/java/by/IT/_02_jpa/repositories/AirplaneRepository.java
+++ src/main/java/by/IT/_02_jpa/repositories/AirplaneRepository.java
@@ -3,7 +3,6 @@ import by.itclass._02_jpa.entities.Airplane;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 @Repository
 public interface AirplaneRepository extends CrudRepository<Airplane, Integer> {
