d8e69b266d4e70a1127d2da04290c6b09f7f8155
Asus <vaxa2320@gmail.com>
2024-11-25 16:25:36 +0300

    user012

--- src/main/java/by/IT/_02_jpa/Main.java
+++ src/main/java/by/IT/_02_jpa/Main.java
@@ -11,18 +11,7 @@ public class Main {
         var ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         var repository = ctx.getBean(AirplaneRepository.class);
         var passRepository = ctx.getBean(PassengerRepository.class);
-//        var boeing736 = ctx.getBean("boeing736", Airplane.class);
-//        var airbus300 = ctx.getBean("airbus300", Airplane.class);
-//        System.out.println("We just created " + boeing736);
-//        boeing736 = repository.save(boeing736);
-//        repository.save(airbus300);
-//        System.out.println("Airplane after save " + boeing736);
-//        boeing736.setPlace(200);
-//        boeing736 = repository.save(boeing736);
-//        System.out.println("Airplane after changes " + boeing736);
-//        var boeing = repository.findByModelLike("Boe%");
-//        System.out.println(boeing);
-//        System.out.println("___________________");
+
 //        var airbus = repository.findByPlaceBetween(250, 500);
 //        airbus.forEach(System.out::println);
 //        repository.findAll().forEach(System.out::println);
