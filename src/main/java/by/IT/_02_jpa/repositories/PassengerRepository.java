525baf8e99bfad65117acd980e232970f8597cc9
Asus <vaxa2320@gmail.com>
2024-12-04 19:04:00 +0300

    user01

--- src/main/java/by/IT/_02_jpa/repositories/PassengerRepository.java
+++ src/main/java/by/IT/_02_jpa/repositories/PassengerRepository.java
@@ -1,9 +1,7 @@
 package by.IT._02_jpa.repositories;
-
 import by.itclass._02_jpa.entities.Passenger;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface PassengerRepository extends CrudRepository<Passenger, Integer> {
 }
