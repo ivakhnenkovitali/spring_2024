ccd41ab6f24a92881505c17e1aa600f490b46b28
Asus <vaxa2320@gmail.com>
2025-01-22 15:34:25 +0300

    user

--- src/2309_03_spring_user-master/src/main/java/by/itclass/model/services/UserService.java
+++ src/2309_03_spring_user-master/src/main/java/by/itclass/model/services/UserService.java
@@ -3,10 +3,7 @@ import by.itclass.model.entities.User;
 import java.util.List;
 public interface UserService {
     List<User> getAllUsers();
-
     void delete(int id);
-
     void add(User user);
-
     void update(User user);
 }
